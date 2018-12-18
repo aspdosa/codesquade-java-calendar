2cb135b29f1d4006bb6054e4bfb87e9c8d1341f9
aspdosa <aspdosa@gmail.com>
2018-12-18 17:51:02 +0900

    반복횟수를 입력받아 해당하는 월의 최대일수를 출력하는 프로그램

--- src/hounx/calendar/Calendar.java
+++ src/hounx/calendar/Calendar.java
@@ -6,17 +6,24 @@ public class Calendar {
     
 	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
 	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month-1];
+		return MAX_DAYS[month -1];
 	}
 	public static void main(String[] args) {
 	
 		//숫자를 입력받아 해당하는 월의 최대일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out .println("달을 입력하세요");
-		int month = scanner.nextInt();
+		System.out .println("반복횟수 입력하세요");
+		int repeat = scanner.nextInt();
+		
+		for (int i =0; i < repeat;i++) {
+			System.out .println("달을 입력하세요");
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal. getMaxDaysOfMonth(month) );
+		}
+		System.out .println("bye ~~~~~");
 		 
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal. getMaxDaysOfMonth(month) );
+		
 		scanner.close();
 	}
 
